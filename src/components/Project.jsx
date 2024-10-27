8976da602fa1b7a5f74a57e86945a096542a4ec9
JIVTESH28 <jivtesh28@gmail.com>
2024-10-27 22:16:35 +0530

    Modified hero and added project section

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,7 +1,16 @@
+import {PROJECTS} from "../projects"
+
 const Project = () => {
   return (
     <div className="border-b border-neutral-900 pb-4">
       <h1 className=" my-20 text-center bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">Projects</h1>
+      <div>
+        {PROJECTS.map((project, index) => (
+          <div key={index}>
+            <img src={project.image} alt={project.title}/>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/projects/index.js
@@ -0,0 +1,27 @@
+import project1 from "../assets/project-1.jpg"
+
+
+
+export const PROJECTS = [
+    {
+      title: "Inventory Management App",
+      image: project1,
+      description:
+        "A fully functional inventory management app with features like product tracking, stock level monitoring, and user authentication.",
+      technologies: ["NextJS", "Tailwindcss", "Express", "Postgresql"],
+    },
+    // {
+    //   title: "Football Analysis System",
+    //   image: project2,
+    //   description:
+    //     "A machine learning-based football analysis system with features like player detection, performance tracking, and ball interpolation",
+    //   technologies: ["ReactJS", "Flask", "Python"],
+    // },
+    // {
+    //   title: "Portfolio Website",
+    //   image: project3,
+    //   description:
+    //     "A personal portfolio website showcasing projects, skills, and contact information.",
+    //   technologies: ["ReactJS", "Tailwindcss", "Framer"],
+    // },
+  ];
