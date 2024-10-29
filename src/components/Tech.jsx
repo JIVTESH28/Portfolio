63b637d676cc30f5a828f4bfa06b3c346e70eada
JIVTESH28 <jivtesh28@gmail.com>
2024-10-29 19:06:06 +0530

    added motion

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,42 +1,98 @@
-import { RiReactjsLine } from "react-icons/ri"
-// import { TbBrandNextjs } from "react-icons/tb" 
-import { SiMongodb } from "react-icons/si"
-import { RiTailwindCssFill } from "react-icons/ri"
-import { FaNodeJs } from "react-icons/fa"
-import { BiLogoPostgresql } from "react-icons/bi"
-import { SiTypescript } from "react-icons/si"
+import { RiReactjsLine } from "react-icons/ri";
+// import { TbBrandNextjs } from "react-icons/tb"
+import { SiMongodb } from "react-icons/si";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { FaNodeJs } from "react-icons/fa";
+import { BiLogoPostgresql } from "react-icons/bi";
+import { SiTypescript } from "react-icons/si";
 // import { IoLogoGithub } from "react-icons/io5"
-import { FaGitAlt } from "react-icons/fa"
+import { FaGitAlt } from "react-icons/fa";
+import { motion } from "framer-motion";
+
+const iconVariants = (duration) => ({
+  initial: { y: -10 },
+  animate: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse",
+    },
+  },
+});
 const Tech = () => {
   return (
     <div className="border-b border-neutral-800 pb-24">
-        <h1 className="my-20 text-center bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">Technologies</h1>
-        <div className="flex flex-wrap items-center justify-center gap-4">
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
-                <RiReactjsLine className="text-7xl text-cyan-400"/>
-            </div>
+      <h1 className="my-20 text-center bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
+        Technologies
+      </h1>
+      <motion.div
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-wrap items-center justify-center gap-4"
+      >
+        <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <RiReactjsLine className="text-7xl text-cyan-400" />
+        </motion.div>
 
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
-                <BiLogoPostgresql className="text-7xl text-sky-700"/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
-                <RiTailwindCssFill className="text-7xl text-cyan-400"/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
-                <FaNodeJs className="text-7xl text-green-500"/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
-                <SiMongodb className="text-7xl text-green-600"/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
-                <SiTypescript className="text-7xl text-sky-600"/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
-                <FaGitAlt className="text-7xl text-orange-600"/>
-            </div>
-        </div>
+        <motion.div
+          variants={iconVariants(5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <BiLogoPostgresql className="text-7xl text-sky-700" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <RiTailwindCssFill className="text-7xl text-cyan-400" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(6)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaNodeJs className="text-7xl text-green-500" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiMongodb className="text-7xl text-green-600" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(7)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiTypescript className="text-7xl text-sky-600" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(4)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaGitAlt className="text-7xl text-orange-600" />
+        </motion.div>
+      </motion.div>
     </div>
-  )
-}
+  );
+};
 
-export default Tech
+export default Tech;
