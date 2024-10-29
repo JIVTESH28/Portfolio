a5e81f364c90eea37b49fa2140c26a08b391711a
JIVTESH28 <jivtesh28@gmail.com>
2024-10-29 18:42:55 +0530

    added motion

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,15 @@
+// import { delay } from "framer-motion";
+import { motion } from "framer-motion";
 import profilepic from "../assets/jivtesh.png";
+
+const container = (delay) => ({
+  hidden: { x: -100, opacity: 0 },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: { duration: 0.5, delay: delay },
+  },
+});
 
 const Hero = () => {
   return (
@@ -6,16 +17,36 @@ const Hero = () => {
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
-            <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
+            <motion.h1
+              variants={container(0)}
+              initial="hidden"
+              animate="visible"
+              className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl"
+            >
               Jivtesh S
-            </h1>
-            <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
+            </motion.h1>
+            <motion.span
+              variants={container(0.5)}
+              initial="hidden"
+              animate="visible"
+              className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent"
+            >
               Full Stack Developer,
-            </span>
-            <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">
+            </motion.span>
+            <motion.span
+              variants={container(1)}
+              initial="hidden"
+              animate="visible"
+              className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent"
+            >
               Machine Learning Developer
-            </span>
-            <p className="my-2 max-w-xl py-6 font-light tracking-tighter">
+            </motion.span>
+            <motion.p
+              variants={container(1.5)}
+              initial="hidden"
+              animate="visible"
+              className="my-2 max-w-xl py-6 font-light tracking-tighter"
+            >
               {" "}
               A machine learning developer with a strong foundation in
               full-stack development, I specialize in creating intelligent
@@ -26,12 +57,12 @@ const Hero = () => {
               integration to responsive front-end interfaces, my work brings
               innovative technology to life through seamless and engaging user
               experiences.
-            </p>
+            </motion.p>
           </div>
         </div>
         <div className="w-full lg:w-1/2 lg:p-8">
           <div className="flex justify-center">
-            <img className="rounded-2xl" src={profilepic} alt="Jivtesh" />
+            <motion.img initial={{x: 100 , opacity: 0}} animate={{x: 0, opacity: 1}} transition={{ duration: 1 , delay: 2}} className="rounded-2xl" src={profilepic} alt="Jivtesh" />
           </div>
         </div>
       </div>
