62c5e6936501abf58d40167478509d423dd68e8e
JIVTESH28 <jivtesh28@gmail.com>
2024-10-30 10:38:13 +0530

    added motion to project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,22 +1,51 @@
-import {PROJECTS} from "../projects"
+import { PROJECTS } from "../projects";
+import { motion } from "framer-motion";
 
 const Project = () => {
   return (
     <div className="border-b border-neutral-900 pb-4">
-      <h1 className=" my-20 text-center bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">Projects</h1>
+      <motion.h1
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 1, y: -100 }}
+        transition={{ duration: 0.5 }}
+        className=" my-20 text-center bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent"
+      >
+        Projects
+      </motion.h1>
       <div>
         {PROJECTS.map((project, index) => (
           <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
-            <div className="w-full lg:w-1/4">
-            <img src={project.image} height={150} width={150} alt={project.title} className="mb-4 rounded" />
-            </div>
-            <div className="w-full max-w-xl lg:w-3/4">
-                <h6 className="mb-2 font-semibold">{project.title}</h6>
-                <p className="mb-4 text-neutral-400">{project.description}</p>
-                {project.technologies.map((tech,index) =>  (
-                  <span key={index} className="mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-cyan-400">{tech}</span>
-               ) )}
-            </div>
+            <motion.div
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 1, x: -100 }}
+              transition={{ duration: 1 }}
+              className="w-full lg:w-1/4"
+            >
+              <img
+                src={project.image}
+                height={150}
+                width={150}
+                alt={project.title}
+                className="mb-4 rounded"
+              />
+            </motion.div>
+            <motion.div
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 1, x: 100 }}
+              transition={{ duration: 1 }}
+              className="w-full max-w-xl lg:w-3/4"
+            >
+              <h6 className="mb-2 font-semibold">{project.title}</h6>
+              <p className="mb-4 text-neutral-400">{project.description}</p>
+              {project.technologies.map((tech, index) => (
+                <span
+                  key={index}
+                  className="mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-cyan-400"
+                >
+                  {tech}
+                </span>
+              ))}
+            </motion.div>
           </div>
         ))}
       </div>
