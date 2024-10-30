1d05b58f3efed14210125c7ec462758e36206c29
JIVTESH28 <jivtesh28@gmail.com>
2024-10-30 10:15:24 +0530

    added motion tech jsx

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -24,9 +24,14 @@ const iconVariants = (duration) => ({
 const Tech = () => {
   return (
     <div className="border-b border-neutral-800 pb-24">
-      <h1 className="my-20 text-center bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
+      <motion.h1
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 1.5 }}
+        className="my-20 text-center bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent"
+      >
         Technologies
-      </h1>
+      </motion.h1>
       <motion.div
         whileInView={{ opacity: 1, x: 0 }}
         initial={{ opacity: 0, x: -100 }}
