b5a4143a65ce50a280543324bd8459d36ab0577d
JIVTESH28 <jivtesh28@gmail.com>
2024-10-30 12:21:06 +0530

    added motion to contact

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { BsTwitterX } from "react-icons/bs";
 
+
 const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
@@ -10,13 +11,12 @@ const Navbar = () => {
         {/* <img className="mx-3 w-10" src={logo} alt="logo" /> */}
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-3xl">
-        <FaLinkedin className="cursor-pointer"/>
+        <FaLinkedin className="cursor-pointer" />
         <FaGithub className="cursor-pointer" />
-        <BsTwitterX  className="cursor-pointer"  />
+        <BsTwitterX className="cursor-pointer" />
       </div>
     </nav>
   );
 };
-
 
 export default Navbar;
